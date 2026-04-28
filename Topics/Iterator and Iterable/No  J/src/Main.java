fa145d69ff0f7e41c88eb955834c5df5b7f13a51
hieudq05 <hieudqpd10569@gmail.com>
2026-04-28 22:22:30 +0700

    feat: add new tasks on processing iterators and collections in "Iterator and Iterable" topic

---
+++ Topics/Iterator and Iterable/No  J/src/Main.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main {
+
+    public static void processIterator(String[] array) {
+        // write your code here
+    }
+
+    /* Do not change code below */
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        processIterator(scanner.nextLine().split(" "));
+    }
+}
